5e95fdc130eb6689f25f79edd7a564dcae04f380
Derrick <dstuckey@lotame.com>
2018-11-25 11:20:21 -0500

    added shaded bars to log(sp price) plot

--- sp_vs_yield_curve_daily.R
+++ sp_vs_yield_curve_daily.R
@@ -24,6 +24,27 @@ nrow(sp.data)
 combined.data$Yield.Curve.Status <- "Positive"
 combined.data$Yield.Curve.Status[combined.data$T10Y2Y.imputed<0] <- "Inverted"
 
+# split the daily data points into 'runs' of positive or inverted yield curve periods
+run=1
+run.start <- combined.data$Date[1]
+current.status=combined.data$Yield.Curve.Status[1]
+last.inversion <- NA
+for(i in 2:nrow(combined.data)) {
+  if(combined.data$Yield.Curve.Status[i]!=current.status) {
+    run <- run+1
+    run.start <- combined.data$Date[i]
+    current.status <- combined.data$Yield.Curve.Status[i]
+    if(combined.data$Yield.Curve.Status[i]=="Inverted") {
+      print(combined.data$Date[i])
+      last.inversion <- combined.data$Date[i]
+    }
+  }
+  combined.data$run[i] <- run
+  combined.data$days.since.start[i] <- combined.data$Date[i] - run.start
+  combined.data$days.since.last.inversion[i] <- combined.data$Date[i] - last.inversion
+  combined.data$last.inversion[i] <- last.inversion
+}
+
 # Yield curve
 # png("./yield_curve_plots_daily/yield_curve_vs_time.png")
 plot(combined.data$Date,combined.data$T10Y2Y.imputed,type="l",
@@ -40,6 +61,7 @@ combined.data$SP.Price.Change.Forward <- (combined.data$Next.Adj.Close / combine
 plot(combined.data$SP.Price.Change.Forward,type="l")
 
 # Forward 1-day S&P price change vs yield curve inversion
+png("./yield_curve_plots_daily/sp_day_price_vs_yc_boxplot.png")
 ggplot(combined.data, aes(x=Yield.Curve.Status, y=SP.Price.Change.Forward)) + 
   ggtitle("Daily S&P Price Change vs\n Inverted Yield Curve") +
   xlab("Yield Curve") + ylab("% Change in S&P 500") + 
@@ -48,8 +70,10 @@ ggplot(combined.data, aes(x=Yield.Curve.Status, y=SP.Price.Change.Forward)) +
                        labels = scales::percent_format(accuracy = 1)) +
   theme(plot.title = element_text(hjust = 0.5)) + 
   stat_summary(fun.y=mean, geom="point", shape=5, size=4)
+dev.off()
 
 # Density Plot
+png("./yield_curve_plots_daily/sp_day_price_vs_yc_density.png")
 ggplot(combined.data, aes(x=SP.Price.Change.Forward,fill=Yield.Curve.Status)) + 
   ggtitle("Daily S&P Price Change vs\n Inverted Yield Curve") +
   xlab("% Change in S&P 500") + ylab("Freqency") + 
@@ -57,6 +81,7 @@ ggplot(combined.data, aes(x=SP.Price.Change.Forward,fill=Yield.Curve.Status)) +
   scale_x_continuous(limits=c(-0.05,0.05), labels = scales::percent_format(accuracy = 1)) + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   labs(fill="Yield Curve")
+dev.off()
 
 summary(combined.data$SP.Price.Change.Forward)
 summary(combined.data$SP.Price.Change.Forward[combined.data$T10Y2Y.imputed<0])
@@ -64,6 +89,9 @@ summary(combined.data$SP.Price.Change.Forward[combined.data$T10Y2Y.imputed>0])
 
 # chi-sq test of positive price change vs positive yield curve
 table(combined.data$SP.Price.Change.Forward>0,combined.data$T10Y2Y.imputed>0)
+# FALSE TRUE
+# FALSE   747 4278
+# TRUE    797 4890
 chisq.test(table(combined.data$SP.Price.Change.Forward>0,combined.data$T10Y2Y.imputed>0))
 # p-value = 0.2209
 
@@ -92,9 +120,8 @@ summary(combined.data$SP.Price.Change.Forward.30Day[combined.data$T10Y2Y.imputed
 summary(combined.data$SP.Price.Change.Forward.30Day[combined.data$T10Y2Y.imputed>0])
 
 # chisq test for forward 30-Day price change
-table(combined.data$SP.Price.Change.Forward.30Day>0,combined.data$T10Y2Y.imputed>0)
-chisq.test(table(combined.data$SP.Price.Change.Forward.30Day>0,combined.data$T10Y2Y.imputed>0))
-# p-value = 0.005698
+# NOT valid as these data points are highly autocorrelated
+# need to segment data points to periods with no overlap in performance period
 
 
 ## Forward 180-Day S&P price change vs yield curve inversion
@@ -121,9 +148,8 @@ summary(combined.data$SP.Price.Change.Forward.180Day[combined.data$T10Y2Y.impute
 summary(combined.data$SP.Price.Change.Forward.180Day[combined.data$T10Y2Y.imputed>0])
 
 # chisq test for forward 180-Day price change
-table(combined.data$SP.Price.Change.Forward.180Day>0,combined.data$T10Y2Y.imputed>0)
-chisq.test(table(combined.data$SP.Price.Change.Forward.180Day>0,combined.data$T10Y2Y.imputed>0))
-
+# NOT valid as these data points are highly autocorrelated
+# need to segment data points to periods with no overlap in performance period
 
 # Boxplot with Yield Curve Buckets
 combined.data$Yield.Curve.Level <- as.factor(paste(round(combined.data$T10Y2Y.imputed,0),"%",sep=""))
@@ -147,48 +173,18 @@ ggplot(combined.data, aes(x=Yield.Curve.Level, y=SP.Price.Change.Forward.30Day))
   theme(plot.title = element_text(hjust = 0.5)) + 
   stat_summary(fun.y=mean, geom="point", shape=5, size=4)
 
-# split the daily data points into 'runs' of positive or inverted yield curve periods
-run=1
-run.start <- combined.data$Date[1]
-current.status=combined.data$Yield.Curve.Status[1]
-last.inversion <- NA
-for(i in 2:nrow(combined.data)) {
-  if(combined.data$Yield.Curve.Status[i]!=current.status) {
-    run <- run+1
-    run.start <- combined.data$Date[i]
-    current.status <- combined.data$Yield.Curve.Status[i]
-    if(combined.data$Yield.Curve.Status[i]=="Inverted") {
-      print(combined.data$Date[i])
-      last.inversion <- combined.data$Date[i]
-    }
-  }
-  combined.data$run[i] <- run
-  combined.data$days.since.start[i] <- combined.data$Date[i] - run.start
-  combined.data$days.since.last.inversion[i] <- combined.data$Date[i] - last.inversion
-  combined.data$last.inversion[i] <- last.inversion
-}
 
-## plot log-scale S&P 500 vs time, highlighting inverted yield curve periods
-# png("./yield_curve_plots_daily/sp_vs_time_yccolor.png")
-ggplot(data=combined.data, aes(x=Date, y=Adj.Close, color=Yield.Curve.Status)) +
-  geom_line(aes(group=run)) +
-  # geom_jitter() + 
-  # geom_point() + 
-  ggtitle("S&P Price vs Time") +
-  xlab("Date") + ylab("S&P 500 (Log-scaled)") + 
-  theme(plot.title = element_text(hjust = 0.5)) + 
-  scale_y_log10() + labs(color="Yield Curve")
-# dev.off()
 
-## plot log-scale S&P 500 vs time, highlighting inverted yield curve periods
-inversions <- subset(combined.data[!is.na(combined.data$days.since.last.inversion) & 
+
+## data manipulation for S&P price change since last inversion
+inversion.starts <- subset(combined.data[!is.na(combined.data$days.since.last.inversion) & 
                                      combined.data$days.since.last.inversion==0,],select=c("Date","Adj.Close"))
-head(inversions)
-names(inversions)[2] <- "Last.Inversion.Adj.Close"
+head(inversion.starts)
+names(inversion.starts)[2] <- "Last.Inversion.Adj.Close"
 after.inversion <- combined.data[!is.na(combined.data$days.since.last.inversion) & 
                                    combined.data$days.since.last.inversion<365*2,]
 head(after.inversion)
-after.inversion <- merge(after.inversion,inversions,by.x="last.inversion",by.y="Date")
+after.inversion <- merge(after.inversion,inversion.starts,by.x="last.inversion",by.y="Date")
 after.inversion <- after.inversion[order(after.inversion$Date),]
 head(after.inversion)
 after.inversion$Price.Change.Since.Last.Inversion <- after.inversion$Adj.Close / after.inversion$Last.Inversion.Adj.Close
@@ -197,8 +193,18 @@ inversion.lengths <- table(after.inversion$last.inversion)
 selected.inversions <- inversion.lengths[inversion.lengths>9]
 summary(as.integer(inversion.lengths))
 
+inversion.periods <- after.inversion[after.inversion$Yield.Curve.Status=="Inverted",]
+inversion.periods <- inversion.periods[order(inversion.periods$Date, decreasing=TRUE),]
+inversion.periods <- inversion.periods[!duplicated(inversion.periods$last.inversion),]
+inversion.periods <- subset(inversion.periods, select=c("Date","last.inversion"))
+names(inversion.periods) <- c("StartDate","EndDate")
+inversion.periods$StartDate <- as.Date(inversion.periods$StartDate)
+inversion.periods$EndDate <- as.Date(inversion.periods$EndDate)
+combined.data$Date <- as.Date(combined.data$Date)
+
+## plot S&P 500 vs days since last inversion
 after.inversion.selected <- after.inversion[after.inversion$last.inversion %in% names(selected.inversions),]
-# png("./yield_curve_plots_daily/sp_price_since_last_inversion")
+png("./yield_curve_plots_daily/sp_price_since_last_inversion")
 ggplot(data=after.inversion.selected, aes(x=days.since.last.inversion, y=Price.Change.Since.Last.Inversion, color=last.inversion)) +
   geom_line(aes(group=run)) +
   # geom_jitter() + 
@@ -207,7 +213,16 @@ ggplot(data=after.inversion.selected, aes(x=days.since.last.inversion, y=Price.C
   xlab("Days Since Last Inversion") + ylab("S&P 500") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   labs(color="Last Inversion") + scale_y_continuous(labels = scales::percent_format(accuracy=1))
-# dev.off()
+dev.off()
 
-
+## plot log-scale S&P 500 vs time, highlighting inverted yield curve periods
+png("./yield_curve_plots_daily/sp_vs_time_yccolor_bars.png")
+ggplot(combined.data) + 
+  geom_line(aes(x=date, y=Adj.Close, color=Yield.Curve.Status, group=run)) + 
+  ggtitle("S&P Price vs Time") + 
+  xlab("Date") + ylab("S&P 500 (Log-scaled)") +
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  scale_y_log10() + labs(color="Yield Curve") + 
+  geom_rect(data=inversion.periods, aes(xmin=StartDate, xmax=EndDate, ymin=0, ymax=+Inf), fill='pink', alpha=0.4)
+dev.off()
 
