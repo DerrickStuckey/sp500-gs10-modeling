3f357a52e3005e11fd62c25a19a4397f8589492c
Derrick <derrickstuckey@gmail.com>
2019-01-07 20:16:38 -0500

    added cumulative log returns for S&P 500, GS10

--- dataprep/calculate_schiller_returns.py
+++ dataprep/calculate_schiller_returns.py
@@ -30,6 +30,31 @@ for i in range(0,nrows,1):
 	except:
 		pass
 
+# log returns
+data['Log.SP.Return.Forward'] = np.log(data['SP.Return.Forward'])
+data['Log.GS10.Return.Forward'] = np.log(data['GS10.Return.Forward'])
+
+# cumulative log returns
+sp_cumulative_log_return = 0
+gs10_cumulative_log_return = 0
+for i in range(0,data.shape[0],1):
+	sp_cumulative_log_return = sp_cumulative_log_return + data.loc[i,'Log.SP.Return.Forward']
+	gs10_cumulative_log_return = gs10_cumulative_log_return + data.loc[i,'Log.GS10.Return.Forward']
+	data.loc[i,'Log.SP.Return.Forward.Cumulative'] = sp_cumulative_log_return
+	data.loc[i,'Log.GS10.Return.Forward.Cumulative'] = gs10_cumulative_log_return
+
+# calculate shifted values for 3-18 months forward
+data['Log.SP.Return.Forward.Cumulative.18Months.Ahead'] = data['Log.SP.Return.Forward.Cumulative'].shift(-18)
+data['Log.SP.Return.Forward.Cumulative.12Months.Ahead'] = data['Log.SP.Return.Forward.Cumulative'].shift(-12)
+data['Log.SP.Return.Forward.Cumulative.6Months.Ahead'] = data['Log.SP.Return.Forward.Cumulative'].shift(-6)
+data['Log.SP.Return.Forward.Cumulative.3Months.Ahead'] = data['Log.SP.Return.Forward.Cumulative'].shift(-3)
+
+data['Log.GS10.Return.Forward.Cumulative.18Months.Ahead'] = data['Log.GS10.Return.Forward.Cumulative'].shift(-18)
+data['Log.GS10.Return.Forward.Cumulative.12Months.Ahead'] = data['Log.GS10.Return.Forward.Cumulative'].shift(-12)
+data['Log.GS10.Return.Forward.Cumulative.6Months.Ahead'] = data['Log.GS10.Return.Forward.Cumulative'].shift(-6)
+data['Log.GS10.Return.Forward.Cumulative.3Months.Ahead'] = data['Log.GS10.Return.Forward.Cumulative'].shift(-3)
+
 # import pdb; pdb.set_trace()
 
+# save the data
 data.to_csv("../prepared_data/schiller_sp_data_withreturns.tsv",index=False,sep="\t")
