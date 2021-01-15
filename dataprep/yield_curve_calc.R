aea5cc3689430bca8dad889b9aaca84f568fe191
Derrick <derrickstuckey@gmail.com>
2021-01-15 18:11:24 -0500

    added yield curve and risk premium factors

---
+++ dataprep/yield_curve_calc.R
@@ -0,0 +1,28 @@
+# Calculates the 10-year Treasury Bond vs 3-month T-Bill spread
+
+# 10-year treasury bond yield
+# from https://fred.stlouisfed.org/series/GS10
+GS10 <- read.csv("./raw_data/GS10.csv", stringsAsFactors = FALSE)
+head(GS10)
+tail(GS10)
+
+# 3-month T-bill rate
+# from https://fred.stlouisfed.org/series/TB3MS
+TB3MS <- read.csv("./raw_data/TB3MS.csv", stringsAsFactors = FALSE)
+head(TB3MS)
+tail(TB3MS)
+names(TB3MS)[2] <- "Tbill.Rate"
+
+combined.data <- merge(GS10, TB3MS, by="DATE")
+# View(combined.data)
+combined.data$DATE <- as.Date(combined.data$DATE, format="%Y-%m-%d")
+
+# calculate the 10-Year T-Bill spread
+combined.data$spread <- combined.data$GS10 - combined.data$Tbill.Rate
+combined.data$Yield.Curve.Status <- "Positive"
+combined.data$Yield.Curve.Status[combined.data$spread<0] <- "Inverted"
+
+write.table(combined.data, "./prepared_data/yield_curve_10y_3mo.tsv",sep="\t",
+            row.names=FALSE)
+
+
--- multifactor/sp_vs_gs10_naive_bayes.R
+++ multifactor/sp_vs_gs10_naive_bayes.R
@@ -4,13 +4,13 @@ library(tidyverse)
 library(lubridate)
 
 # S&P 500 and 10-year treasury returns data
-# monthly since 1871
+# from monthly since 1871
 sp.data <- read.csv("./prepared_data/schiller_sp_data_withreturns.tsv", sep="\t", stringsAsFactors = FALSE)
 head(sp.data)
 tail(sp.data)
 sp.data$Date <- as.Date(sp.data$Date, format="%Y-%m-%d")
 
-## S&P forward return vs individual factors:
+### S&P forward return vs individual factors:
 
 # 6-month price momentum
 sp.data$SP.Momentum.6Mo <- sp.data$SP.Price - sp.data$SP.Price.Prev.6Mo
@@ -20,14 +20,24 @@ boxplot(sp.data$SP.Return.Forward-1 ~ sp.data$SP.Momentum.6Mo>0, outline=FALSE)
 sp.data$SP.Momentum.12Mo <- sp.data$SP.Price - sp.data$SP.Price.Prev.12Mo
 boxplot(sp.data$SP.Return.Forward-1 ~ sp.data$SP.Momentum.12Mo>0, outline=FALSE)
 
-# Commercial and Industrial Loan growth
+
+## Yield Curve (10-year 3-month spread, monthly)
+# from dataprep/yield_curve_calc.R
+yield.curve.data <- read.csv("./prepared_data/yield_curve_10y_3mo.tsv",sep="\t")
+head(yield.curve.data)
+yield.curve.data$DATE <- as.Date(yield.curve.data$DATE, format="%Y-%m-%d")
+sp.data.yc <- sp.data %>% inner_join(yield.curve.data, by=c("Date"="DATE"))
+# sp.data.yc %>%
+#   select(Date,SP.Price,GS10.x,spread) %>%
+#   View()
+
+# view the relationship between positive / inverted spreads and S&P forward returns
+boxplot(sp.data.yc$SP.Return.Forward-1 ~ sp.data.yc$Yield.Curve.Status)
 
 
-# Yield Curve
-
-
-# Sentiment
+## Sentiment
 sentiment.data <- read.csv("./prepared_data/sentiment_data_formatted.tsv", sep="\t", stringsAsFactors = FALSE)
+head(sentiment.data)
 tail(sentiment.data)
 # clean up dates
 sentiment.data$ReportedDate <- lubridate::mdy(sentiment.data$ReportedDate)
@@ -48,9 +58,19 @@ sp.data.sentiment %>%
   select(Bullish, Log.SP.Return.Forward) %>%
   drop_na() %>%
   cor()
+boxplot(sp.data.sentiment$Log.SP.Return.Forward ~ sp.data.sentiment$Bullish > mean(sp.data.sentiment$Bullish))
 
-# Valuation (Risk Premium)
+## Valuation (Risk Premium)
+sp.data$Earnings.Yield <- sp.data$Earnings / sp.data$SP.Price
+sp.data$SP.Risk.Premium <- sp.data$Earnings.Yield - sp.data$GS10/100
+summary(sp.data$SP.Risk.Premium)
+hist(sp.data$SP.Risk.Premium)
+
+# check out the relationship between risk premium and forward S&P returns
+plot(y=sp.data$SP.Return.Forward-1, x=sp.data$SP.Risk.Premium)
+sp.data %>% select(SP.Return.Forward, SP.Risk.Premium) %>%
+  drop_na() %>%
+  cor()
 
 
-# Change in Interest Rates
 
