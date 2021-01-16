755cab92ddfa6f6f258f83f37c18c6025a5c7850
Derrick <derrickstuckey@gmail.com>
2021-01-16 10:03:15 -0500

    up-front training/test split

--- multifactor/sp_vs_gs10_naive_bayes.R
+++ multifactor/sp_vs_gs10_naive_bayes.R
@@ -10,15 +10,19 @@ head(sp.data)
 tail(sp.data)
 sp.data$Date <- as.Date(sp.data$Date, format="%Y-%m-%d")
 
+sp.data.train <- sp.data %>% filter(Date < "2005-01-01")
+sp.data.test <- sp.data %>% filter(Date >= "2005-01-01")
+tail(sp.data.train)
+
 ### S&P forward return vs individual factors:
 
 # 6-month price momentum
-sp.data$SP.Momentum.6Mo <- sp.data$SP.Price - sp.data$SP.Price.Prev.6Mo
-boxplot(sp.data$SP.Return.Forward-1 ~ sp.data$SP.Momentum.6Mo>0, outline=FALSE)
+sp.data.train$SP.Momentum.6Mo <- sp.data.train$SP.Price - sp.data.train$SP.Price.Prev.6Mo
+boxplot(sp.data.train$SP.Return.Forward-1 ~ sp.data.train$SP.Momentum.6Mo>0, outline=FALSE)
 
 # 12-month price momentum
-sp.data$SP.Momentum.12Mo <- sp.data$SP.Price - sp.data$SP.Price.Prev.12Mo
-boxplot(sp.data$SP.Return.Forward-1 ~ sp.data$SP.Momentum.12Mo>0, outline=FALSE)
+sp.data.train$SP.Momentum.12Mo <- sp.data.train$SP.Price - sp.data.train$SP.Price.Prev.12Mo
+boxplot(sp.data.train$SP.Return.Forward-1 ~ sp.data.train$SP.Momentum.12Mo>0, outline=FALSE)
 
 
 ## Yield Curve (10-year 3-month spread, monthly)
@@ -26,13 +30,13 @@ boxplot(sp.data$SP.Return.Forward-1 ~ sp.data$SP.Momentum.12Mo>0, outline=FALSE)
 yield.curve.data <- read.csv("./prepared_data/yield_curve_10y_3mo.tsv",sep="\t")
 head(yield.curve.data)
 yield.curve.data$DATE <- as.Date(yield.curve.data$DATE, format="%Y-%m-%d")
-sp.data.yc <- sp.data %>% inner_join(yield.curve.data, by=c("Date"="DATE"))
-# sp.data.yc %>%
+sp.data.train.yc <- sp.data.train %>% inner_join(yield.curve.data, by=c("Date"="DATE"))
+# sp.data.train.yc %>%
 #   select(Date,SP.Price,GS10.x,spread) %>%
 #   View()
 
 # view the relationship between positive / inverted spreads and S&P forward returns
-boxplot(sp.data.yc$SP.Return.Forward-1 ~ sp.data.yc$Yield.Curve.Status)
+boxplot(sp.data.train.yc$SP.Return.Forward-1 ~ sp.data.train.yc$Yield.Curve.Status)
 
 
 ## Sentiment
@@ -49,28 +53,44 @@ sentiment.data.monthly <- sentiment.data %>%
 sentiment.data.monthly <- sentiment.data.monthly %>%
   distinct(CeilingMonth, .keep_all = TRUE)
 # merge with S&P Data
-sp.data.sentiment <- sp.data %>%
+sp.data.train.sentiment <- sp.data.train %>%
   inner_join(sentiment.data.monthly, by=c("Date"="CeilingMonth"))
 
 # analyze the relationship between Bullish Sentiment and 1-month forward S&P 500 Return
-plot(sp.data.sentiment$Bullish, sp.data.sentiment$Log.SP.Return.Forward)
-sp.data.sentiment %>%
+plot(sp.data.train.sentiment$Bullish, sp.data.train.sentiment$Log.SP.Return.Forward)
+sp.data.train.sentiment %>%
   select(Bullish, Log.SP.Return.Forward) %>%
   drop_na() %>%
   cor()
-boxplot(sp.data.sentiment$Log.SP.Return.Forward ~ sp.data.sentiment$Bullish > mean(sp.data.sentiment$Bullish))
+boxplot(sp.data.train.sentiment$Log.SP.Return.Forward ~ sp.data.train.sentiment$Bullish > mean(sp.data.train.sentiment$Bullish))
 
 ## Valuation (Risk Premium)
-sp.data$Earnings.Yield <- sp.data$Earnings / sp.data$SP.Price
-sp.data$SP.Risk.Premium <- sp.data$Earnings.Yield - sp.data$GS10/100
-summary(sp.data$SP.Risk.Premium)
-hist(sp.data$SP.Risk.Premium)
+# sp.data.train$Earnings.Yield <- sp.data.train$Earnings / sp.data.train$SP.Price
+sp.data.train$Earnings.Yield <- 1/sp.data.train$CAPE
+sp.data.train$SP.Risk.Premium <- sp.data.train$Earnings.Yield - sp.data.train$GS10
+summary(sp.data.train$SP.Risk.Premium)
+hist(sp.data.train$SP.Risk.Premium)
 
 # check out the relationship between risk premium and forward S&P returns
-plot(y=sp.data$SP.Return.Forward-1, x=sp.data$SP.Risk.Premium)
-sp.data %>% select(SP.Return.Forward, SP.Risk.Premium) %>%
+plot(y=sp.data.train$SP.Return.Forward-1, x=sp.data.train$SP.Risk.Premium)
+sp.data.train %>% select(SP.Return.Forward, SP.Risk.Premium) %>%
   drop_na() %>%
   cor()
 
+# try against real interest rates
+sp.data.train$CPI.12mo.ago <- lag(sp.data.train$CPI,12)
+# sp.data.train %>% select(CPI, CPI.12mo.ago) %>% View()
+sp.data.train$CPI.YoY.Change <- sp.data.train$CPI / sp.data.train$CPI.12mo.ago - 1
+tail(sp.data.train$CPI.YoY.Change)
+sp.data.train$GS10.Real <- sp.data.train$GS10 - sp.data.train$CPI.YoY.Change
+hist(sp.data.train$GS10.Real)
+sp.data.train$SP.Risk.Premium.Real <- sp.data.train$Earnings.Yield - sp.data.train$GS10.Real
 
+# relationship between real risk premium and forward S&P returns
+plot(y=sp.data.train$SP.Return.Forward-1, x=sp.data.train$SP.Risk.Premium.Real)
+sp.data.train %>% select(SP.Return.Forward, SP.Risk.Premium.Real) %>%
+  drop_na() %>%
+  cor()
+
+# try using T-bill rate
 
