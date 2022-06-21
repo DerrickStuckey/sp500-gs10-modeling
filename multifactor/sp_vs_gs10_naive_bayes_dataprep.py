bdbedeac09bab46f35990c39abb0322088a59fa3
Derrick <derrickstuckey@gmail.com>
2022-06-21 09:50:17 -0400

    treasury spread calc

--- multifactor/sp_vs_gs10_naive_bayes_dataprep.py
+++ multifactor/sp_vs_gs10_naive_bayes_dataprep.py
@@ -23,10 +23,6 @@ schiller_data['SP.Momentum.6Mo.Negative'][-100:-1].value_counts()
 (schiller_data['SP.Price.Prev.12Mo'] < schiller_data['SP.Price'])[-100:-1].value_counts()
 schiller_data['SP.Momentum.12Mo.Negative'][-100:-1].value_counts()
 
-## Yield Curve Status
-yield_curve_fname = "../prepared_data/yield_curve_10y_3mo.tsv"
-yield_curve_data = pd.read_csv(yield_curve_fname, sep="\t")
-
 ## AAII Sentiment Data
 aaii_filename = "../prepared_data/sentiment_data_formatted.tsv"
 aaii_data = pd.read_csv(aaii_filename, sep="\t")
@@ -41,30 +37,33 @@ aaii_data_monthly = aaii_data_reverse[aaii_data_reverse_dupl==False].sort_index(
 
 ## T-bill rate for risk premium calculation
 TB3MS = pd.read_csv("../raw_data/TB3MS.csv")
+TB3MS['Tbill.Rate'] = np.divide(TB3MS['TB3MS'],100)
 
-# format dates???
-# schiller_data.dtypes
-# yield_curve_data.dtypes
+## 10-year treasury data series for treasury spread (aka yield curve)
+GS10 = pd.read_csv("../raw_data/GS10.csv")
+GS10['Treasury.10Y.Rate'] = np.divide(GS10['GS10'],100)
+
+# format dates
+schiller_data.dtypes
+GS10.dtypes
 schiller_data = schiller_data.astype({"Date": np.datetime64})
-yield_curve_data = yield_curve_data.astype({"DATE": np.datetime64})
-
+GS10 = GS10.astype({"DATE": np.datetime64})
 TB3MS = TB3MS.astype({"DATE": np.datetime64})
 
 ## join all the tables
 
-# join schiller data and yield curve
-joined_data = pd.merge(schiller_data, yield_curve_data, how="left", left_on="Date", right_on="DATE")
-
+# join 10-year treasury data
+joined_data = pd.merge(schiller_data, GS10, how="left", left_on="Date", right_on="DATE")
 # verify the join
 schiller_data.shape
 joined_data.shape
 schiller_data['Date'].tail()
-yield_curve_data['DATE'].head()
-yield_curve_data['DATE'].tail()
-joined_data[['Date','Yield.Curve.Status']].head()
-joined_data[['Date','Yield.Curve.Status']].tail()
-yield_curve_data['Yield.Curve.Status'].value_counts()
-joined_data['Yield.Curve.Status'].value_counts()
+GS10['DATE'].head()
+GS10['DATE'].tail()
+joined_data[['Date','Treasury.10Y.Rate']].head()
+joined_data[['Date','Treasury.10Y.Rate']].tail()
+GS10['Treasury.10Y.Rate'].describe()
+joined_data['Treasury.10Y.Rate'].describe()
 
 # add t-bill rate data
 joined_data = pd.merge(joined_data, TB3MS, how="left", left_on="Date", right_on="DATE")
@@ -73,10 +72,7 @@ joined_data[['Date','TB3MS']].head()
 joined_data[['Date','TB3MS']].tail()
 joined_data['TB3MS'].describe()
 TB3MS['TB3MS'].describe()
-joined_data['Tbill.Rate'] = np.divide(joined_data['TB3MS'],100)
 joined_data['Tbill.Rate'].tail()
-
-# TODO add in 10-year treasury data and calculate yield curve here
 
 # add aaii data
 aaii_data_monthly.head()
@@ -86,6 +82,16 @@ joined_data[['Date','Bullish']].head()
 joined_data[['Date','Bullish']].tail()
 aaii_data_monthly['Bullish'].describe()
 joined_data['Bullish'].describe()
+
+# TODO calculate yield curve
+# import pdb; pdb.set_trace()
+joined_data['GS10.Tbill.Spread'] = np.subtract(joined_data['Treasury.10Y.Rate'], joined_data['Tbill.Rate'])
+joined_data['GS10.Tbill.Spread'].describe()
+joined_data['GS10.Tbill.Spread.Positive'] = np.sign(joined_data['GS10.Tbill.Spread'])
+joined_data['GS10.Tbill.Spread.Positive'].value_counts()
+# consider 0 spread to be negative
+joined_data['GS10.Tbill.Spread.Positive'].mask(joined_data['GS10.Tbill.Spread.Positive']<0,0,inplace=True)
+joined_data['GS10.Tbill.Spread.Positive'].value_counts()
 
 # calculate risk premium
 # use CAPE for earnings yield for smoothing
@@ -97,8 +103,6 @@ joined_data[['Date','SP.Risk.Premium.Tbill','Earnings.Yield','Tbill.Rate']].tail
 joined_data['Low.Risk.Premium'] = np.sign(np.subtract(0.01,joined_data['SP.Risk.Premium.Tbill']))
 # consider cases where risk premium = 0.01 to be high risk premium
 joined_data['Low.Risk.Premium'].mask(joined_data['Low.Risk.Premium']<0,0,inplace=True)
-
-import pdb; pdb.set_trace()
 
 # Construct the target variable
 # For a Naive Bayes model we need a categorical target, so just use a binary variable
@@ -113,4 +117,6 @@ joined_data['SP.Outperforms.GS10'].mask(joined_data['SP.Outperforms.GS10']<0,0,i
 # verify
 joined_data[['Date','SP.Outperforms.GS10','SP.Return.Forward','GS10.Return.Forward']].tail()
 joined_data['SP.Outperforms.GS10'].value_counts()
+
+joined_data.to_csv('./naive_bayes_model_training_data.tsv',sep="\t",index=False)
 
